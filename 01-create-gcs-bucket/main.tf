1039de0f967bd3b58066d27f82fc35ae0bdb1326
Garikina Babji <garikina.babji@gmail.com>
2026-07-14 15:30:35 +0530

    Create main.tf

---
+++ 01-create-gcs-bucket/main.tf
@@ -0,0 +1,15 @@
+
+
+
+provider "google" {
+  project = "babji-product-2615"
+  region  = "asia-south1"
+}
+
+resource "google_storage_bucket" "raw_bucket" {
+  name          = "babji-raw-data-2615"
+  location      = "ASIA-SOUTH1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
