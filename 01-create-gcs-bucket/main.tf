1c5deda6803f4aebd6d0150f0a67c3a41d3bcaba
Garikina Babji <garikina.babji@gmail.com>
2026-07-14 15:41:20 +0530

    Add Google Cloud Storage bucket resource
    Added a new Google Cloud Storage bucket resource definition.

--- 01-create-gcs-bucket/main.tf
+++ 01-create-gcs-bucket/main.tf
@@ -13,3 +13,14 @@ resource "google_storage_bucket" "raw_bucket" {
 
   uniform_bucket_level_access = true
 }
+
+# or you can this also
+
+resource "google_storage_bucket" "raw_bucket" {
+  name     = var.bucket_name
+  location = "ASIA-SOUTH1"
+
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
